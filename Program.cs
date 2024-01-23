19a1d877ffc2acf98ca56cd4e892ddc53eee479c
rondastar <116147793+rondastar@users.noreply.github.com>
2024-01-22 16:38:43 -0800

    Edited bubble sort to accept generic arrays; added a test array of strings

--- Program.cs
+++ Program.cs
@@ -8,14 +8,19 @@
         static void Main(string[] args)
         {
             // Sample array to test bubble sort
-            int[] array = { 7, 3, 98, 72, 45, 12, 96, 4, 16, 86, 82, 63, 97, 76, 23 };
-            PrintArray(array);
-            BubbleSort(array);
-            PrintArray(array);
+            int[] intArray = { 7, 3, 98, 72, 45, 12, 96, 4, 16, 86, 82, 63, 97, 76, 23 };
+            PrintArray(intArray);
+            BubbleSort(intArray);
+            PrintArray(intArray);
+            // Sample array to test bubble sort
+            string[] stringArray = { "aardvark", "zebra", "moon", "run", "cat", "yak", "tortoise", "pig" };
+            PrintArray(stringArray);
+            BubbleSort(stringArray);
+            PrintArray(stringArray);
 
         } // Main
 
-        static void BubbleSort(int[] array )
+        static void BubbleSort<T>(T[] array )
         {
             // Get the length of the input array
             int arrayLenth = array.Length;
@@ -24,32 +29,30 @@
             bool swapOccurred = true;
 
             // Continue iterating until no swaps occur in a pass
-            while (swapOccurred)
+            do
             {
                 // Reset the swap flag at the start of each pass
                 swapOccurred = false;
 
                 // Iterate through the array elements from the first to the second-to-last
-                // element for currentIndex from 1 to arrayLength - 1
-                // NOTE: I did it as instructed the first time, but the last element wasn't
-                // sorted, so I changed the code to loop through the last element too
-                for (int i = 1; i < arrayLenth; i++)
+                // element for current Index from 0 to arrayLength - 1
+                for (int i = 0; i < arrayLenth - 1; i++)
                 {
                     // Compare the current element with the next element
-                    if (array[i - 1] > array[i])
+                    if (Comparer<T>.Default.Compare(array[i], array[i + 1]) > 0)
                     {
                         // Swap the elements if they are out of order
-                        int temp = array[i];
-                        array[i] = array[i - 1];
-                        array[i - 1] = temp;
+                        T temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
 
                         // Set the swap flag to true to indicate a swap occurred
                         swapOccurred = true;
                     } // if 
                 } // for loop
-            } // while loop
+            } while (swapOccurred) ; // while loop
         }
-        static void PrintArray(int[] array)
+        static void PrintArray<T>(T[] array)
         {
             Console.Write("Array: ");
             foreach (var item in array)
